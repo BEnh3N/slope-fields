02eb8486bc6247cf35cd41a2c6b2837194d4d772
BEnh3N <bhpohlmann@icloud.com>
2024-01-06 11:09:53 -0800

    added slope visual

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
+use std::f32::consts::PI;
 // use std::f32::consts::E;
-
-use error_iter::ErrorIter as _;
-use log::error;
-use pixels::{Error, Pixels, SurfaceTexture};
+use pixels::{Pixels, SurfaceTexture};
 use winit::dpi::PhysicalSize;
 use winit::event::{Event, VirtualKeyCode};
-use winit::event_loop::{ControlFlow, EventLoop};
+use winit::event_loop::{EventLoop};
 use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 
@@ -14,24 +12,24 @@ const HEIGHT: u32 = 1000;
 const W2: f32 = WIDTH as f32 / 2.;
 const H2: f32 = HEIGHT as f32 / 2.;
 
-struct World {}
+struct World {
+    mouse_pos: (f32, f32)
+}
 
 fn dx_dy(x: f32, y: f32) -> f32 {
     // 2. * x - y
-    // 2. - (x * y)
+    // x * y
     // x + y
-    // -x / y
+    -x / y
     // (3. * x.powi(2) + 1.) / (2. * y)
-    x + y
 }
 
 fn sigmoid(x: f32) -> f32 {
     // 1. / (1. + E.powf(-x))
-    (x / 3.).tanh()
+    (x * 0.25).tanh()
 }
 
-fn main() -> Result<(), Error> {
-    env_logger::init();
+fn main()  {
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
     let window = {
@@ -47,7 +45,7 @@ fn main() -> Result<(), Error> {
     let mut pixels = {
         let window_size = window.inner_size();
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+        Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap()
     };
     let mut world = World::new();
 
@@ -56,8 +54,8 @@ fn main() -> Result<(), Error> {
         if let Event::RedrawRequested(_) = event {
             world.draw(pixels.frame_mut());
             if let Err(err) = pixels.render() {
-                log_error("pixels.render", err);
-                *control_flow = ControlFlow::Exit;
+                eprintln!("{}", err);
+                control_flow.set_exit();
                 return;
             }
         }
@@ -66,8 +64,13 @@ fn main() -> Result<(), Error> {
         if input.update(&event) {
             // Close events
             if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
-                *control_flow = ControlFlow::Exit;
+                control_flow.set_exit();
                 return;
+            }
+
+            match input.mouse() {
+                Some(pos) => world.mouse_pos = pos,
+                None => {}
             }
 
             // Resize the window
@@ -86,16 +89,11 @@ fn main() -> Result<(), Error> {
     });
 }
 
-fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
-    error!("{method_name}() failed: {}", err);
-    for source in err.sources().skip(1) {
-        error!("  Caused by: {source}");
-    }
-}
-
 impl World {
     fn new() -> Self {
-        Self {}
+        Self {
+            mouse_pos: (0., 0.)
+        }
     }
 
     fn update(&mut self) {}
@@ -104,9 +102,7 @@ impl World {
         for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
             let x = (i % WIDTH as usize) as i32;
             let y = (i / WIDTH as usize) as i32;
-
-            let rx = ((x as f32 - W2) / W2) * 10.;
-            let ry = (-(y as f32 - H2) / H2) * 10.;
+            let (rx, ry) = pixel_to_grid_space(x, y);
 
             let m = dx_dy(rx, ry);
 
@@ -118,11 +114,95 @@ impl World {
                 rgba[2] = (-g * 255.0) as u8
             }
 
-            // if m.abs() >= 0.95 && m.abs() <= 1.05 {
-            //     rgba = [0xff, 0xff, 0xff, 0xff]
-            // }
-
             pixel.copy_from_slice(&rgba);
+        }
+
+        for i in 0..=18 {
+            for j in 0..=18 {
+                let rx = (i - 9) as f32;
+                let ry = (j - 9) as f32;
+                draw_slope_line(frame, rx, ry, 40., true);
+            }
+        }
+
+        let x = self.mouse_pos.0 as i32;
+        let y = self.mouse_pos.1 as i32;
+        let (rx, ry) = pixel_to_grid_space(x, y);
+        draw_slope_line(frame, rx, ry, 100., true);
+    }
+}
+
+fn pixel_to_grid_space(x: i32, y: i32) -> (f32, f32) {
+    let new_x = ((x as f32 - W2) / W2) * 10.;
+    let new_y = (-(y as f32 - H2) / H2) * 10.;
+    (new_x, new_y)
+}
+
+fn grid_to_pixel_space(x: f32, y: f32) -> (i32, i32) {
+    let new_x = (((x / 10.) * W2) + W2) as i32;
+    let new_y = ((-(y / 10.) * H2) + H2) as i32;
+    (new_x, new_y)
+}
+
+fn draw_slope_line(frame: &mut [u8], x: f32, y: f32, len: f32, draw_arrow: bool) {
+    let (px, py) = grid_to_pixel_space(x, y);
+    let m = dx_dy(x, y);
+    let a = m.atan();
+
+    let r = len / 2.;
+
+    let dx = (r * a.cos()) as i32;
+    let dy = (r * a.sin()) as i32;
+
+    draw_line(frame, px + dx, py + dy, px - dx, py - dy);
+
+    if draw_arrow {
+        let offset = 3. * PI / 4.;
+        let a2 = a + offset;
+        let r2 = 7.5;
+        let dx2 = (r2 * a2.cos()) as i32;
+        let dy2 = (r2 * a2.sin()) as i32;
+        draw_line(frame, px+dx, py+dy, px+dx+dx2, py+dy+dy2);
+
+        let a3 = a - offset;
+        let dx3 = (r2 * a3.cos()) as i32;
+        let dy3 = (r2 * a3.sin()) as i32;
+        draw_line(frame, px+dx, py+dy, px+dx+dx3, py+dy+dy3);
+    }
+}
+
+fn draw_line(frame: &mut [u8], x1: i32, y1: i32, x2: i32, y2: i32) {
+    let mut x1 = x1 as f32;
+    let mut y1 = y1 as f32;
+    let x2 = x2 as f32;
+    let y2 = y2 as f32;
+
+    let dx = (x2 - x1).abs();
+    let sx = if x1 < x2 { 1. } else { -1. };
+    let dy = -(y2 - y1).abs();
+    let sy = if y1 < y2 { 1. } else { -1. };
+    let mut error = dx + dy;
+
+    loop {
+        let x = x1 as usize;
+        let y = y1 as usize;
+        if x < WIDTH as usize && y < HEIGHT as usize {
+            let i = y * WIDTH as usize + x;
+            let pixel = frame.chunks_exact_mut(4).nth(i).unwrap();
+            pixel.copy_from_slice(&[255, 255, 255, 255]);
+        }
+
+        if x1 == x2 && y1 == y2 { break }
+        let e2 = error * 2.;
+        if e2 >= dy {
+            if x1 == x2 { break }
+            error += dy;
+            x1 += sx;
+        }
+        if e2 <= dx {
+            if y1 == y2 { break }
+            error += dx;
+            y1 += sy;
         }
     }
 }
